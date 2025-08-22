8a1f4b5133126be35460da0dfbe5317dc8ab6803
Felix Palmer <felixpalmer@gmail.com>
2025-08-22 08:22:30 +0200

    Port CRS

--- src/projections/mod.rs
+++ src/projections/mod.rs
@@ -3,9 +3,11 @@
 // Copyright (c) A5 contributors
 
 pub mod authalic;
+pub mod crs;
 pub mod gnomonic;
 pub mod polyhedral;
 
 pub use authalic::AuthalicProjection;
+pub use crs::CRS;
 pub use gnomonic::GnomonicProjection;
 pub use polyhedral::PolyhedralProjection;
