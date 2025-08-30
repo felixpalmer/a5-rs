53b468f7efa6b2820a21d42e129107c2b1551997
Felix Palmer <felixpalmer@gmail.com>
2025-08-30 15:46:01 +0200

    public modules only for tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,23 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (c) A5 contributors
 
+// Internal modules - public only for testing
+#[cfg_attr(not(test), allow(unused))]
 pub mod coordinate_systems;
+#[cfg_attr(not(test), allow(unused))]
 pub mod core;
+#[cfg_attr(not(test), allow(unused))]
 pub mod geometry;
+#[cfg_attr(not(test), allow(unused))]
 pub mod projections;
+#[cfg_attr(not(test), allow(unused))]
 pub mod utils;
 
-#[cfg(test)]
-mod test;
 
 // PUBLIC API
 // Indexing
 pub use core::cell::{cell_to_boundary, cell_to_lonlat, lonlat_to_cell};
-pub use core::hex::{hex_to_u64, u64_to_hex};
+pub use core::hex::{hex_to_big_int, big_int_to_hex, u64_to_hex};
 
 // Hierarchy
 pub use core::serialization::{cell_to_parent, cell_to_children, get_resolution, get_res0_cells};
